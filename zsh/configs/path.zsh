f6da2256da7bb9f8bb199a8e3aeaf0c3e6285a5f
Albert Kiskorov <albi@Alberts-MacBook-Pro.local>
2023-12-09 23:25:32 +0700

    Fix if zsh statement in path.zsh

--- zsh/configs/path.zsh
+++ zsh/configs/path.zsh
@@ -2,45 +2,45 @@
 PATH="$HOME/.bin:/usr/local/sbin:$PATH"
 
 # Try loading ASDF from the regular home dir location
-if [ -f "$HOME/.asdf/asdf.sh" ]; then
+if [[ -f "$HOME/.asdf/asdf.sh" ]]; then
   . "$HOME/.asdf/asdf.sh"
 elif which brew >/dev/null; then
   . "$(brew --prefix asdf)/libexec/asdf.sh"
 fi
 
-if [ -d "$HOME/.deno/bin/deno" ]; then
+if [[ -d "$HOME/.deno/bin/deno" ]]; then
   PATH="$HOME/.deno/bin:$PATH"
 fi
 
-if [ -d "/opt/homebrew/opt/ruby/bin/ruby" ]; then
+if [[ -d "/opt/homebrew/opt/ruby/bin/ruby" ]]; then
   PATH="/opt/homebrew/opt/ruby/bin:$PATH"
 fi
 
-if [ -d "$HOME/go/bin" ]; then
+if [[ -d "$HOME/go/bin" ]]; then
   PATH="$HOME/go/bin:$PATH"
 fi
 
-if [ -d "/opt/homebrew/opt/python/bin" ]; then
+if [[ -d "/opt/homebrew/opt/python/bin" ]]; then
   PATH="/opt/homebrew/opt/python/bin:$PATH"
 fi
 
-if [ -d "/opt/homebrew/opt/node/bin" ]; then
+if [[ -d "/opt/homebrew/opt/node/bin" ]]; then
   PATH="/opt/homebrew/opt/node/bin:$PATH"
 fi
 
-if [ -d "/opt/homebrew/opt/npm/bin" ]; then
+if [[ -d "/opt/homebrew/opt/npm/bin" ]]; then
   PATH="/opt/homebrew/opt/npm/bin:$PATH"
 fi
 
-if [ -d "/opt/homebrew/opt/yarn/bin" ]; then
+if [[ -d "/opt/homebrew/opt/yarn/bin" ]]; then
   PATH="/opt/homebrew/opt/yarn/bin:$PATH"
 fi
 
-if [ -d "/opt/homebrew/opt/pipx/bin" ]; then
+if [[ -d "/opt/homebrew/opt/pipx/bin" ]]; then
   PATH="/opt/homebrew/opt/pipx/bin:$PATH"
 fi
 
-if [ -d "$HOME/.cargo/bin" ]; then
+if [[-d "$HOME/.cargo/bin" ]]; then
   PATH="$HOME/.cargo/bin:$PATH"
 fi
 
