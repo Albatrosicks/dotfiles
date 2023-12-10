15319b383556762f4d90d81c44b26fdffd9cd363
Albert Kiskorov <albi@Alberts-MacBook-Pro.local>
2023-12-11 00:18:44 +0700

    Remove bash completion in yandex-cloud config

--- zsh/configs/10-yandex-cloud.zsh
+++ zsh/configs/10-yandex-cloud.zsh
@@ -1,6 +1,2 @@
 # The next line updates PATH for Yandex Cloud CLI.
 if [ -f "$HOME/yandex-cloud/path.bash.inc" ]; then source "$HOME/yandex-cloud/path.bash.inc"; fi
-
-# The next line enables shell command completion for yc.
-if [ -f "$HOME/yandex-cloud/completion.zsh.inc" ]; then source "$HOME/yandex-cloud/completion.zsh.inc"; fi
-
