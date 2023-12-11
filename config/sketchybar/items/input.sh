e384c680d8e70079b45456ed69829e4eaecb7fc3
Albert Kiskorov <albi@Alberts-MacBook-Pro.local>
2023-12-11 21:11:25 +0700

    Change keyboard layout detection mechanism

--- config/sketchybar/items/input.sh
+++ config/sketchybar/items/input.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env sh
 
-sketchybar --add alias "TextInputMenuAgent" right \
-	--set "TextInputMenuAgent" icon.padding_left=-18 \
-	--add event keyboard_change "AppleSelectedInputSourcesChangedNotification" \
-	label.padding_right=-18 \
-	alias.color=0xfff0f0f0 \
-	background.padding_left=-1 \
-	background.padding_right=-1 \
-	--subscribe keyboard keyboard_change
+keyboard=(
+    icon.drawing=off
+    label.font="$FONT:Semibold:14.0"
+    script="$PLUGIN_DIR/keyboard.sh"
+)
 
+sketchybar --add item keyboard right        \
+           --set keyboard "${keyboard[@]}"  \
+           --add event keyboard_change "AppleSelectedInputSourcesChangedNotification" \
+           --subscribe keyboard keyboard_change
