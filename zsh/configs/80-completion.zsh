551fb9ed90b79db15f17dbd3a7efc732dd95da96
Albert Kiskorov <albi@Alberts-MacBook-Pro.local>
2024-01-03 22:16:00 +0700

    Add vagrant completions to fpath

--- zsh/configs/80-completion.zsh
+++ zsh/configs/80-completion.zsh
@@ -1,3 +1,4 @@
 # load our own completion functions
 fpath=($HOME/.zsh/completion /usr/local/share/zsh/site-functions $fpath)
+fpath=(/opt/vagrant/embedded/gems/gems/vagrant-2.4.0/contrib/zsh $fpath)
 export -U fpath
