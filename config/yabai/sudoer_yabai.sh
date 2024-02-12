77628ea23cb9a567bdd6866961d6a3f2cb7e8daa
Albert Kiskorov <albi@Alberts-MacBook-Pro.local>
2024-02-12 22:10:44 +0700

    Add another sudoers path to yabai conf

--- config/yabai/sudoer_yabai.sh
+++ config/yabai/sudoer_yabai.sh
@@ -8,3 +8,5 @@ echo "$WHOAMI ALL=(root) NOPASSWD: sha256:$YABAI_HASH $YABAI_PATH --load-sa"
 echo
 echo "Open visudo and paste the above line into the file."
 echo "\$ sudo visudo -f /etc/private/sudoers.d/yabai"
+echo "or"
+echo "\$ sudo visudo -f /etc/sudoers.d/yabai"
