a3e5625adceb7d3a1932f22bbacde5cc7f0a37b0
Albert Kiskorov <albi@Alberts-MacBook-Pro.local>
2024-02-23 16:50:07 +0700

    Add zinit

---
+++ zsh/configs/95-zinit-plugins.zsh
@@ -0,0 +1,27 @@
+zinit load gangleri/pipenv
+
+zinit load lukechilds/zsh-better-npm-completion
+zinit load greymd/docker-zsh-completion
+zinit load srijanshetty/zsh-pip-completion
+zinit load esc/conda-zsh-completion
+zinit load MichaelAquilina/zsh-autoswitch-virtualenv
+zinit load laggardkernel/git-ignore
+zinit load zpm-zsh/ssh
+zinit load lukechilds/zsh-nvm
+zinit load Ynjxsjmh/zsh-poetry
+zinit load maximux13/zsh-auto-source-file
+zinit load Albatrosicks/go-task-completions
+zinit load helix-editor/helix path:contrib/completion kind:fpath
+zinit load rgcr/m-cli path:completion/zsh kind:fpath
+zinit load joshskidmore/zsh-fzf-history-search
+
+# initilize broken complitions (doesn't work before plugin manager)
+zinit load akoenig/npm-run.plugin.zsh
+
+zinit load zsh-users/zsh-completions
+zinit load Aloxaf/fzf-tab
+zinit load zsh-users/zsh-syntax-highlighting
+
+autoload -Uz compinit
+compinit
+zinit cdreplay -q
