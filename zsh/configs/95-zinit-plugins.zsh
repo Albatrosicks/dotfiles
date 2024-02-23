8dc880d13e05446f3721b221680dd598b3c7f8f1
Albert Kiskorov <albi@Alberts-MacBook-Pro.local>
2024-02-23 17:49:25 +0700

    Fix zinit plugins

--- zsh/configs/95-zinit-plugins.zsh
+++ zsh/configs/95-zinit-plugins.zsh
@@ -1,24 +1,29 @@
-zinit load gangleri/pipenv
+zinit wait lucid light-mode for \
+  atinit"ZINIT[COMPINIT_OPTS]=-C; zicompinit; zicdreplay" zdharma/fast-syntax-highlighting \
+  blockf zsh-users/zsh-completions \
+  atload"!_zsh_autosuggest_start; export ZSH_AUTOSUGGEST_USE_ASYNC=1" zsh-users/zsh-autosuggestions \
+  Aloxaf/fzf-tab \
+  MichaelAquilina/zsh-autoswitch-virtualenv \
+  laggardkernel/git-ignore \
+  zpm-zsh/ssh \
+  lukechilds/zsh-nvm \
+  Ynjxsjmh/zsh-poetry \
+  maximux13/zsh-auto-source-file \
+  Albatrosicks/go-task-completions \
+  joshskidmore/zsh-fzf-history-search \
+  akoenig/npm-run.plugin.zsh \
+  zsh-users/zsh-syntax-highlighting \
+  atload"typeset -g ZSH_SYSTEM_CLIPBOARD_TMUX_SUPPORT='true'" kutsan/zsh-system-clipboard
 
-zinit load lukechilds/zsh-better-npm-completion
-zinit load greymd/docker-zsh-completion
-zinit load srijanshetty/zsh-pip-completion
-zinit load esc/conda-zsh-completion
-zinit load MichaelAquilina/zsh-autoswitch-virtualenv
-zinit load laggardkernel/git-ignore
-zinit load zpm-zsh/ssh
-zinit load lukechilds/zsh-nvm
-zinit load Ynjxsjmh/zsh-poetry
-zinit load maximux13/zsh-auto-source-file
-zinit load Albatrosicks/go-task-completions
-zinit load joshskidmore/zsh-fzf-history-search
+zinit ice wait"2" as"command" from"gh-r" lucid \
+  mv"zoxide*/zoxide -> zoxide" \
+  atclone"./zoxide init zsh > init.zsh" \
+  atpull"%atclone" src"init.zsh" nocompile'!'
+zinit light ajeetdsouza/zoxide
 
-# initilize broken complitions (doesn't work before plugin manager)
-zinit load akoenig/npm-run.plugin.zsh
+zinit ice from"gh-r" as"program" atload'eval "$(starship init zsh)"'
+zinit load starship/starship
 
-zinit load zsh-users/zsh-completions
-zinit load Aloxaf/fzf-tab
-zinit load zsh-users/zsh-syntax-highlighting
 
 autoload -Uz compinit
 compinit
