a405f0d34644887e7e1948422c26fc43292e25b1
Albert Kiskorov <albi@Alberts-MacBook-Pro.local>
2024-02-23 12:41:00 +0700

    Enable completions in completion stage

--- zsh/configs/80-completion.zsh
+++ zsh/configs/80-completion.zsh
@@ -3,3 +3,5 @@ fpath=($HOME/.zsh/completion /usr/local/share/zsh/site-functions $fpath)
 fpath=(/opt/vagrant/embedded/gems/gems/vagrant-2.4.0/contrib/zsh $fpath)
 fpath=($(brew --prefix)/share/zsh/site-functions $fpath)
 export -U fpath
+
+autoload -U compinit && compinit
