568d8dc38e6817dffdd387b544cb3382b71aa574
Albert Kiskorov <albi@Alberts-MacBook-Pro.local>
2024-02-23 15:11:35 +0700

    Remove compdef from 80-completion again

--- zsh/configs/80-completion.zsh
+++ zsh/configs/80-completion.zsh
@@ -3,6 +3,3 @@ fpath=($HOME/.zsh/completion /usr/local/share/zsh/site-functions $fpath)
 fpath=(/opt/vagrant/embedded/gems/gems/vagrant-2.4.0/contrib/zsh $fpath)
 fpath=($(brew --prefix)/share/zsh/site-functions $fpath)
 export -U fpath
-
-autoload -Uz compinit
-compinit
