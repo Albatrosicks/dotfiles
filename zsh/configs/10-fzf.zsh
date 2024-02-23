4b09cb6ad15a68284904c70fff8d50a7d5e21c76
Albert Kiskorov <albi@Alberts-MacBook-Pro.local>
2024-02-23 11:54:49 +0700

    completes the common prefix if it is also a match

--- zsh/configs/10-fzf.zsh
+++ zsh/configs/10-fzf.zsh
@@ -1,2 +1,3 @@
 export FZF_BASE="/opt/local/share/fzf"
 export FZF_DEFAULT_OPTS="--ansi --preview-window 'right:60%' --preview 'bat --color=always --style=header,grid --line-range :300 {}'"
+export FZF_COMPLETION_AUTO_COMMON_PREFIX=true
