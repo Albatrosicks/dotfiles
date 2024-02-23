d3aa57944a58906aee5f6916bd4458264adc22ad
Albert Kiskorov <albi@Alberts-MacBook-Pro.local>
2024-02-23 12:25:22 +0700

    add brew completions

--- zsh/configs/80-completion.zsh
+++ zsh/configs/80-completion.zsh
@@ -1,4 +1,5 @@
 # load our own completion functions
 fpath=($HOME/.zsh/completion /usr/local/share/zsh/site-functions $fpath)
 fpath=(/opt/vagrant/embedded/gems/gems/vagrant-2.4.0/contrib/zsh $fpath)
+fpath=($(brew --prefix)/share/zsh/site-functions $fpath)
 export -U fpath
