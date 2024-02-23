e16f20956f1e6faa2051feabe2e1db7369b9d29b
Albert Kiskorov <albi@Alberts-MacBook-Pro.local>
2024-02-23 17:19:57 +0700

    add removed completions

--- zsh/configs/80-completion.zsh
+++ zsh/configs/80-completion.zsh
@@ -1,6 +1,7 @@
 # load our own completion functions
 fpath=($HOME/.zsh/completion /usr/local/share/zsh/site-functions $fpath)
 fpath=(/opt/vagrant/embedded/gems/gems/vagrant-2.4.0/contrib/zsh $fpath)
-fpath=($(ls -d -1 /opt/homebrew/Cellar/helix/*/share/zsh/site-functions | head -n  1) $fpath)
+fpath=(/opt/homebrew/Cellar/helix/*/share/zsh/site-functions $fpath)
+fpath=(/opt/homebrew/Cellar/m-cli/*/share/zsh/site-functions $fpath)
 fpath=($(brew --prefix)/share/zsh/site-functions $fpath)
 export -U fpath
