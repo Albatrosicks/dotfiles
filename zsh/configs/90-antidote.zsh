37cde1f6cc028a9c59463044a91d8cf19a973197
Albert Kiskorov <albi@Alberts-MacBook-Pro.local>
2024-02-23 16:49:49 +0700

    Remove antibody

--- zsh/configs/90-antidote.zsh
+++
@@ -1,7 +0,0 @@
-zsh_plugins=${ZDOTDIR:-$HOME}/.zsh_plugins
-if [[ ! ${zsh_plugins}.zsh -nt ${zsh_plugins}.txt ]]; then
-  source /opt/homebrew/opt/antidote/share/antidote/antidote.zsh
-  antidote bundle <${zsh_plugins}.txt >${zsh_plugins}.zsh
-fi
-
-source ${zsh_plugins}.zsh
