75f6fc007bbfba0bcd2aa7d00ff84537f0ce6958
Albert Kiskorov <albi@Alberts-MacBook-Pro.local>
2024-02-23 22:45:29 +0700

    remove zoxide from history ignore

--- zsh/configs/10-history.zsh
+++ zsh/configs/10-history.zsh
@@ -2,6 +2,6 @@ setopt hist_ignore_all_dups inc_append_history
 HISTFILE=~/.zhistory
 HISTSIZE=4096
 SAVEHIST=4096
-HISTORY_IGNORE="(ls|cd|pwd|exit|ll|history|ez|eza|z)"
+HISTORY_IGNORE="(ls|cd|pwd|exit|ll|history|ez|eza)"
 
 export ERL_AFLAGS="-kernel shell_history enabled"
