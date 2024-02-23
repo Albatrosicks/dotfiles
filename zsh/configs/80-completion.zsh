6944bf86cc4413c3ca96b965a1df7a20bade2931
Albert Kiskorov <albi@Alberts-MacBook-Pro.local>
2024-02-23 17:16:42 +0700

    add helix editor to fpath

--- zsh/configs/80-completion.zsh
+++ zsh/configs/80-completion.zsh
@@ -1,5 +1,6 @@
 # load our own completion functions
 fpath=($HOME/.zsh/completion /usr/local/share/zsh/site-functions $fpath)
 fpath=(/opt/vagrant/embedded/gems/gems/vagrant-2.4.0/contrib/zsh $fpath)
+fpath=($(ls -d -1 /opt/homebrew/Cellar/helix/*/share/zsh/site-functions | head -n  1) $fpath)
 fpath=($(brew --prefix)/share/zsh/site-functions $fpath)
 export -U fpath
