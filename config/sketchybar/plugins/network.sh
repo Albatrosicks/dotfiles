66f0a7a39fa351ab177f4e7badf1a1c5e329efd6
Albert Kiskorov <albi@Alberts-MacBook-Pro.local>
2024-06-07 01:33:12 +0700

    Back to network graph

--- config/sketchybar/plugins/network.sh
+++ config/sketchybar/plugins/network.sh
@@ -1,22 +1,9 @@
 #!/usr/bin/env bash
-
+MAX_DOWN=110000
+MAX_UP=45000
 UPDOWN=$(ifstat -i "en0" -b 0.1 1 | tail -n1)
 DOWN=$(echo $UPDOWN | awk "{ print \$1 }" | cut -f1 -d ".")
 UP=$(echo $UPDOWN | awk "{ print \$2 }" | cut -f1 -d ".")
 
-DOWN_FORMAT=""
-if [ "$DOWN" -gt "999" ]; then
-  DOWN_FORMAT=$(echo $DOWN | awk '{ printf "%03.0f Mbps", $1 / 1000}')
-else
-  DOWN_FORMAT=$(echo $DOWN | awk '{ printf "%03.0f kbps", $1}')
-fi
-
-UP_FORMAT=""
-if [ "$UP" -gt "999" ]; then
-  UP_FORMAT=$(echo $UP | awk '{ printf "%03.0f Mbps", $1 / 1000}')
-else
-  UP_FORMAT=$(echo $UP | awk '{ printf "%03.0f kbps", $1}')
-fi
-
-sketchybar -m --set network_down label="$DOWN_FORMAT" icon.highlight=$(if [ "$DOWN" -gt "0" ]; then echo "on"; else echo "off"; fi) \
-                    --set network_up label="$UP_FORMAT" icon.highlight=$(if [ "$UP" -gt "0" ]; then echo "on"; else echo "off"; fi)
+sketchybar -m --push network_down $(echo $UPDOWN | awk "{ print \$1 / (1.0 * $MAX_DOWN) }")
+              --push network_up $(echo $UPDOWN | awk "{ print \$2 / (1.0 * $MAX_UP) }")
