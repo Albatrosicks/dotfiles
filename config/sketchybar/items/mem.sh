46f124ed258c20a23bc7f16c4b8db2b4ef7aae7b
Albert Kiskorov <albi@Alberts-MacBook-Pro.local>
2024-06-07 01:01:01 +0700

    Add width

--- config/sketchybar/items/mem.sh
+++ config/sketchybar/items/mem.sh
@@ -1,8 +1,8 @@
 sketchybar -m --add item topmem right \
               --set topmem label.font="$FONT:Regular:7.0" \
-                                    y_offset=5 \
-                                    update_freq=1 \
-                                    script="~/.config/sketchybar/plugins/topmem.sh" \ 
+                               y_offset=5 \
+                               width=0 \
+                               script="~/.config/sketchybar/plugins/topmem.sh" \
 \
               --add item ram_percentage right \
               --set ram_percentage label.font="$FONT:Regular:7.0" \
