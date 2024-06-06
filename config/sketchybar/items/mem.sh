94b9c14ddd95d0c62bfd84dbf5fd854a7cd53f31
Albert Kiskorov <albi@Alberts-MacBook-Pro.local>
2024-06-07 00:54:01 +0700

    Fix typo

--- config/sketchybar/items/mem.sh
+++ config/sketchybar/items/mem.sh
@@ -3,7 +3,7 @@ sketchybar -m --add item topmem_app right \
                                     y_offset=5 \
                                     update_freq=1 \
                                     script="~/.config/sketchybar/plugins/topmem.sh" \ 
-
+\
               --add item ram_percentage right \
               --set ram_percentage label.font="$FONT:Regular:7.0" \
                                     y_offset=-4 \
