e09cfa1a23097e4d11babfc460e2e424dfd40b05
Albert Kiskorov <albi@Alberts-MacBook-Pro.local>
2024-06-07 14:27:40 +0700

    Revert "Change network layout"
    This reverts commit 058a6802d53452457b76f63f27d8fcb25c114f65.

--- config/sketchybar/helper/network.h
+++ config/sketchybar/helper/network.h
@@ -5,15 +5,11 @@
 #include <stdbool.h>
 
 #define MAX_INTERFACES 10
-#define MBIT_TO_BYTES 1250000 // 10 Mbit/s = 1250000 bytes/s
 
 struct network {
   unsigned long prev_bytes_in[MAX_INTERFACES];
   unsigned long prev_bytes_out[MAX_INTERFACES];
   bool has_prev_data;
-
-  unsigned long max_speed_in;
-  unsigned long max_speed_out;
 
   char command[256];
 };
@@ -22,8 +18,6 @@ static inline void network_init(struct network* net) {
   memset(net->prev_bytes_in, 0, sizeof(net->prev_bytes_in));
   memset(net->prev_bytes_out, 0, sizeof(net->prev_bytes_out));
   net->has_prev_data = false;
-  net->max_speed_in = MBIT_TO_BYTES;
-  net->max_speed_out = MBIT_TO_BYTES;
   snprintf(net->command, 100, "");
 }
 
@@ -58,25 +52,22 @@ static inline void network_update(struct network* net) {
     unsigned long delta_in = bytes_in - net->prev_bytes_in[0];
     unsigned long delta_out = bytes_out - net->prev_bytes_out[0];
 
-    double download_speed = (double)delta_in; // Speed in bytes/s
-    double upload_speed = (double)delta_out; // Speed in bytes/s
+    double download_speed = (double)delta_in / 1048576.0; // Convert to MB
+    double upload_speed = (double)delta_out / 1048576.0; // Convert to MB
 
-    // Update max speed if current speed is higher
-    if (download_speed > net->max_speed_in) net->max_speed_in = download_speed;
-    if (upload_speed > net->max_speed_out) net->max_speed_out = upload_speed;
-
-    // Calculate percentage
-    double download_perc = (download_speed / (net->max_speed_in > MBIT_TO_BYTES ? net->max_speed_in : MBIT_TO_BYTES)) * 100;
-    double upload_perc = (upload_speed / (net->max_speed_out > MBIT_TO_BYTES ? net->max_speed_out : MBIT_TO_BYTES)) * 100;
+    char color[16];
+    snprintf(color, 16, "%s", getenv("LABEL_COLOR"));
 
     snprintf(net->command, 256, "--push network.down %.2f "
                                 "--push network.up %.2f "
-                                "--set network.down label='Download: %.2f MB/s' "
-                                "--set network.up label='Upload: %.2f MB/s' ",
-                                download_perc,
-                                upload_perc,
-                                download_speed / 1048576.0,
-                                upload_speed / 1048576.0);
+                                "--set network.down label='Download: %.2f MB/s' label.color=%s "
+                                "--set network.up label='Upload: %.2f MB/s' label.color=%s ",
+                                download_speed,
+                                upload_speed,
+                                download_speed,
+                                color,
+                                upload_speed,
+                                color);
   } else {
     snprintf(net->command, 256, "");
   }
