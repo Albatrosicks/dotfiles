38c2e64a56c2ce2abbeedd1602ab78237e204817
Albert Kiskorov <albi@Alberts-MacBook-Pro.local>
2024-06-07 14:27:55 +0700

    Revert "Change network widget to C version"
    This reverts commit 5f589e64e5d142f23d9dd91620e14d7382dd6962.

--- config/sketchybar/items/net.sh
+++ config/sketchybar/items/net.sh
@@ -1,53 +1,17 @@
-#!/bin/bash
-
-net_top=(
-  label.font="$FONT:Semibold:7"
-  label=NET
-  icon.drawing=off
-  width=0
-  padding_right=15
-  y_offset=6
-)
-
-net_percent=(
-  label.font="$FONT:Heavy:12"
-  label=NET
-  y_offset=-4
-  padding_right=15
-  width=55
-  icon.drawing=off
-  update_freq=4
-  mach_helper="$HELPER"
-)
-
-net_in=(
-  width=0
-  graph.color=$BLUE
-  graph.fill_color=$BLUE
-  label.drawing=off
-  icon.drawing=off
-  background.height=30
-  background.drawing=on
-  background.color=$TRANSPARENT
-)
-
-net_out=(
-  graph.color=$RED
-  label.drawing=off
-  icon.drawing=off
-  background.height=30
-  background.drawing=on
-  background.color=$TRANSPARENT
-)
-
-sketchybar --add item net.top right              \
-           --set net.top "${net_top[@]}"         \
-                                                 \
-           --add item net.percent right          \
-           --set net.percent "${net_percent[@]}" \
-                                                 \
-           --add graph net.in right 75           \
-           --set net.in "${net_in[@]}"           \
-                                                 \
-           --add graph net.out right 75          \
-           --set net.out "${net_out[@]}"
+sketchybar -m --add item network_up right \
+              --set network_up label.font="$FONT:Regular:7.0" \
+                               icon.font="$FONT:Regular:7.0" \
+                               icon= \
+                               icon.highlight_color=0xff8b0a0d \
+                               y_offset=6 \
+                               width=0 \
+                               update_freq=1 \
+                               script="~/.config/sketchybar/plugins/network.sh" \
+\
+              --add item network_down right \
+              --set network_down label.font="$FONT:Regular:7.0" \
+                                 icon.font="$FONT:Regular:7.0" \
+                                 icon= \
+                                 icon.highlight_color=0xff10528c \
+                                 y_offset=-4 \
+                                 update_freq=1
