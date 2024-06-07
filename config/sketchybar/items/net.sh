ad356808b0886f66f3f003fd156ba861e1b57318
Albert Kiskorov <albi@Alberts-MacBook-Pro.local>
2024-06-07 14:32:03 +0700

    Remove network widget icons

--- config/sketchybar/items/net.sh
+++ config/sketchybar/items/net.sh
@@ -1,14 +1,3 @@
-sketchybar -m --add item net right \
-              --set net script="~/.config/sketchybar/plugins/net.sh" \
-                                background.color=0xff3B4252 \
-                                background.height=20
-
-sketchybar -m --add item net_logo right \
-              --set net_logo icon= \
-                        background.color=0xffB48EAD \
-                        background.height=20 \
-                        background.padding_left=5
-
 sketchybar -m --add item network_up right \
               --set network_up label.font="$FONT:Regular:7.0" \
                                icon.font="$FONT:Regular:7.0" \
