51909ac1e27e302f93981be2513fab88b0d44fa3
Albert Kiskorov <me@albi.io>
2024-06-24 18:02:35 +0700

    [yabai] typo

--- config/yabai/sudoer_yabai.sh
+++ config/yabai/sudoer_yabai.sh
@@ -7,6 +7,6 @@ WHOAMI=$(whoami)
 echo "$WHOAMI ALL=(root) NOPASSWD: sha256:$YABAI_HASH $YABAI_PATH --load-sa"
 echo
 echo "Open visudo and paste the above line into the file."
-echo "\$ sudo visudo -f /private/sudoers.d/yabai"
+echo "\$ sudo visudo -f /private/etc/sudoers.d/yabai"
 echo "or"
 echo "\$ sudo visudo -f /etc/sudoers.d/yabai"
