691a09a8c16e71b226544a76ea92463d6d2886c3
Albert Kiskorov <me@albi.io>
2024-06-24 18:45:52 +0700

    [skhd] Change layout switch to ENG/RUS for PC

--- config/skhd/scripts/switch_layout.sh
+++ config/skhd/scripts/switch_layout.sh
@@ -2,12 +2,12 @@
 
 current_layout=$(macism)
 
-if [ "$current_layout" == "com.apple.keylayout.ABC" ]
+if [ "$current_layout" == "com.apple.keylayout.USInternational-PC" ]
 then
-  macism com.apple.keylayout.Russian
-elif [ "$current_layout"  == "com.apple.keylayout.Russian" ]
+  macism com.apple.keylayout.RussianWin
+elif [ "$current_layout"  == "com.apple.keylayout.RussianWin" ]
 then
-  macism com.apple.keylayout.ABC
+  macism com.apple.keylayout.USInternational-PC
 else
   echo "Unknown layout: $current_layout"
 fi
