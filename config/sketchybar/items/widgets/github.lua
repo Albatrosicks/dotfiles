c3a48ff9925f0318600af2fab8ad5596d4f6a244
Albert Kiskorov <me@albi.io>
2024-07-21 22:33:42 +0700

    [sketchybar] Change github widget

--- config/sketchybar/items/widgets/github.lua
+++ config/sketchybar/items/widgets/github.lua
@@ -5,212 +5,159 @@ local settings = require("settings")
 local popup_off = "sketchybar --set github popup.drawing=off"
 
 local github = sbar.add("item", "github", {
-	position = "right",
-	icon = {
-		string = icons.bell,
-		color = colors.blue,
-		font = {
-			family = settings.font,
-			style = "Bold",
-			size = 15.0,
-		},
-	},
-	background = {
-		padding_left = 0,
-	},
-	label = {
-		string = icons.loading,
-		highlight_color = colors.blue,
-	},
-	update_freq = 180,
-	popup = {
-		align = "right",
-	},
+    position = "right",
+    icon = {
+        string = icons.bell,
+        color = colors.blue,
+        font = {
+            family = settings.font,
+            style = "Bold",
+            size = 15.0,
+        },
+    },
+    background = {
+        padding_left = 0,
+    },
+    label = {
+        string = icons.loading,
+        highlight_color = colors.blue,
+    },
+    update_freq = 180,
+    popup = {
+        align = "right",
+    },
 })
 
-github.details = sbar.add("item", "github.details", {
-	position = "popup." .. github.name,
-	click_script = popup_off,
-	background = {
-		corner_radius = 12,
-		padding_left = 7,
-		padding_right = 7,
-	},
-	icon = {
-		background = {
-			height = 2,
-			y_offset = -12,
-		},
-	},
+local github_details = sbar.add("item", "github.details", {
+    position = "popup.github",
+    click_script = popup_off,
+    background = {
+        corner_radius = 12,
+        padding_left = 7,
+        padding_right = 7,
+    },
+    icon = {
+        background = {
+            height = 2,
+            y_offset = -12,
+        },
+    },
 })
 
-github:subscribe({
-	"mouse.clicked",
-}, function(info)
-	if info.BUTTON == "left" then
-		POPUP_TOGGLE(info.NAME)
-	end
+local github_bracket = sbar.add("bracket", "github.bracket", {
+    github.name,
+    github_details.name
+}, {
+    background = { color = colors.bg1 },
+    popup = { align = "center", height = 30 }
+})
 
-	if info.BUTTON == "right" then
-		sbar.trigger("github_update")
-	end
+local function toggle_popup(info)
+    local should_draw = github_bracket:query().popup.drawing == "off"
+    github_bracket:set({ popup = { drawing = should_draw } })
+end
+
+github:subscribe("mouse.clicked", function(info)
+    if info.BUTTON == "left" then
+        toggle_popup(info)
+    elseif info.BUTTON == "right" then
+        sbar.trigger("github_update")
+    end
 end)
 
-github:subscribe({
-	"mouse.exited",
-	"mouse.exited.global",
-}, function(_)
-	github:set({ popup = { drawing = false } })
+github:subscribe({"mouse.exited", "mouse.exited.global"}, function(_)
+    github:set({ popup = { drawing = false } })
 end)
 
-github:subscribe({
-	"mouse.entered",
-}, function(_)
-	github:set({ popup = { drawing = true } })
+github:subscribe("mouse.entered", function(_)
+    github:set({ popup = { drawing = true } })
 end)
 
-github:subscribe({
-	"routine",
-	"forced",
-	"github_update",
-}, function(_)
-	-- fetch new information
-	sbar.exec("gh api notifications", function(notifications)
-		-- Clear existing packages
-		local existingNotifications = github:query()
-		if existingNotifications.popup and next(existingNotifications.popup.items) ~= nil then
-			for _, item in pairs(existingNotifications.popup.items) do
-				sbar.remove(item)
-			end
-		end
+github:subscribe({"routine", "forced", "github_update"}, function(_)
+    sbar.exec("gh api notifications", function(notifications)
+        local existingNotifications = github:query()
+        if existingNotifications.popup and next(existingNotifications.popup.items) ~= nil then
+            for _, item in pairs(existingNotifications.popup.items) do
+                sbar.remove(item)
+            end
+        end
 
-		-- PRINT_TABLE(notifications)
+        local count = 0
+        for _, notification in pairs(notifications) do
+            count = count + 1
 
-		local count = 0
-		for _, notification in pairs(notifications) do
-			-- increment count for label
-			count = count + 1
+            local id = notification.id
+            local url = notification.subject.latest_comment_url or "https://www.github.com/notifications"
+            local repo = notification.repository.name
+            local title = notification.subject.title
+            local type = notification.subject.type
 
-			local id = notification.id
-			local url = notification.subject.latest_comment_url
-			local repo = notification.repository.name
-			local title = notification.subject.title
-			local type = notification.subject.type
+            local color, icon
+            if type == "Issue" then
+                color = colors.green
+                icon = icons.git.issue
+            elseif type == "Discussion" then
+                color = colors.text
+                icon = icons.git.discussion
+            elseif type == "PullRequest" then
+                color = colors.maroon
+                icon = icons.git.pull_request
+            elseif type == "Commit" then
+                color = colors.text
+                icon = icons.git.commit
+            else
+                color = colors.text
+                icon = icons.git.issue
+            end
 
-			-- set click_script for each notification
-			if url == nil then
-				url = "https://www.github.com/notifications"
-			else
-				local tempUrl = url:gsub("^'", ""):gsub("'$", "")
-				sbar.exec('gh api "' .. tempUrl .. '" | jq .html_url', function(html_url)
-					if IS_EMPTY(repo) == false then
-						sbar.exec(
-							"sketchybar -m --set github.notification.repo"
-								.. tostring(id)
-								.. ' click_script="open '
-								.. html_url
-								.. '"',
-							function()
-								sbar.exec(popup_off)
-							end
-						)
-					end
+            if repo then
+                sbar.add("item", "github.notification.repo" .. tostring(id), {
+                    label = { padding_right = settings.paddings },
+                    icon = {
+                        string = icon .. " " .. repo,
+                        color = color,
+                        font = {
+                            family = settings.nerd_font,
+                            size = 14.0,
+                            style = "Bold",
+                        },
+                        padding_left = settings.paddings,
+                    },
+                    drawing = true,
+                    click_script = "open " .. url .. "; " .. popup_off,
+                    position = "popup.github",
+                })
+            end
 
-					if IS_EMPTY(title) == false then
-						sbar.exec(
-							"sketchybar -m --set github.notification.message."
-								.. tostring(id)
-								.. ' click_script="open '
-								.. html_url
-								.. '"',
-							function()
-								sbar.exec(popup_off)
-							end
-						)
-					end
-				end)
-			end
+            if title then
+                sbar.add("item", "github.notification.message." .. tostring(id), {
+                    label = {
+                        string = title,
+                        padding_right = 10,
+                    },
+                    icon = {
+                        drawing = "off",
+                        padding_left = settings.paddings,
+                    },
+                    drawing = true,
+                    click_script = "open " .. url .. "; " .. popup_off,
+                    position = "popup.github",
+                })
+            end
+        end
 
-			-- get icon and color for each notification
-			-- depending on the type
-			local color, icon
-			if type == "Issue" then
-				color = colors.green
-				icon = icons.git.issue
-			elseif type == "Discussion" then
-				color = colors.text
-				icon = icons.git.discussion
-			elseif type == "PullRequest" then
-				color = colors.maroon
-				icon = icons.git.pull_request
-			elseif type == "Commit" then
-				color = colors.text
-				icon = icons.git.commit
-			else
-				color = colors.text
-				icon = icons.git.issue
-			end
+        github:set({
+            icon = { string = icons.bell_dot },
+            label = 0,
+        })
 
-			-- add notification to popup
-			github.notification = {}
-
-			if IS_EMPTY(repo) == false then
-				github.notification.repo = sbar.add("item", "github.notification.repo" .. tostring(id), {
-					label = {
-						padding_right = settings.paddings,
-					},
-					icon = {
-						string = icon .. " " .. repo,
-						color = color,
-						font = {
-							family = settings.nerd_font,
-							size = 14.0,
-							style = "Bold",
-						},
-						padding_left = settings.paddings,
-					},
-					drawing = true,
-					-- TODO: trigger update after clicking since notification is cleared on github
-					click_script = "open " .. url .. "; " .. popup_off,
-					position = "popup." .. github.name,
-				})
-			end
-
-			if IS_EMPTY(title) == false then
-				github.notification.message = sbar.add("item", "github.notification.message." .. tostring(id), {
-					label = {
-						string = title,
-						padding_right = 10,
-					},
-					icon = {
-						drawing = "off",
-						padding_left = settings.paddings,
-					},
-					drawing = true,
-					-- TODO: trigger update after clicking since notification is cleared on github
-					click_script = "open " .. url .. "; " .. popup_off,
-					position = "popup." .. github.name,
-				})
-			end
-		end
-
-		-- Change icon and color depending on packages
-		github:set({
-			icon = {
-				string = icons.bell_dot,
-			},
-			label = 0,
-		})
-
-		if count > 0 then
-			github:set({
-				icon = {
-					string = icons.bell,
-				},
-				label = count,
-			})
-		end
-	end)
+        if count > 0 then
+            github:set({
+                icon = { string = icons.bell },
+                label = count,
+            })
+        end
+    end)
 end)
 
 return github
