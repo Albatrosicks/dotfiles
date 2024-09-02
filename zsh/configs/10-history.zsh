712601929d922cf010962148ae2a094054398cbb
Albert Kiskorov <me@albi.io>
2024-09-02 11:56:26 +0700

    Add export to history ignore

--- zsh/configs/10-history.zsh
+++ zsh/configs/10-history.zsh
@@ -2,6 +2,6 @@ setopt hist_ignore_all_dups inc_append_history
 HISTFILE=~/.zhistory
 HISTSIZE=4096
 SAVEHIST=4096
-HISTORY_IGNORE="(ls|cd|pwd|exit|ll|history|ez|eza)"
+HISTORY_IGNORE="(ls|cd|pwd|exit|ll|history|ez|eza|export)"
 
 export ERL_AFLAGS="-kernel shell_history enabled"
