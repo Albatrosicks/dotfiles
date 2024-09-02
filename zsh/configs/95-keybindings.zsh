0d07612135802c91f02503971b1a3cf2fa7a9d8c
Albert Kiskorov <me@albi.io>
2024-09-02 12:31:44 +0700

    update keybindings for shell

--- zsh/configs/95-keybindings.zsh
+++ zsh/configs/95-keybindings.zsh
@@ -1,7 +1,14 @@
 # handy keybindings
-bindkey "^A" beginning-of-line
-bindkey "^E" end-of-line
-bindkey "^K" kill-line
-bindkey "^P" history-search-backward
-bindkey "^Y" accept-and-hold
-bindkey "^N" insert-last-word
+bindkey "^K"      kill-whole-line                      # ctrl-k
+bindkey "^R"      history-incremental-search-backward  # ctrl-r
+bindkey "^A"      beginning-of-line                    # ctrl-a
+bindkey "^E"      end-of-line                          # ctrl-e
+bindkey "^P"      history-search-backward              # ctrl-p
+bindkey "^Y"      accept-and-hold                      # ctrl-y
+bindkey "^N"      insert-last-word                     # ctrl-n
+bindkey "^D"      delete-char                          # ctrl-d
+bindkey "^F"      forward-char                         # ctrl-f
+bindkey "^B"      backward-char                        # ctrl-b
+bindkey "^[[1;4B" backward-word                        # alt-shift-b
+bindkey "^[[1;4F" forward-word                         # alt-shift-f
+bindkey -v   # Default to standard vi bindings, regardless of editor string
