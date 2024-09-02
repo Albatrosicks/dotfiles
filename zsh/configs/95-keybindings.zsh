c78921f68470c13bea217c1ed677e70e26f40acd
Albert Kiskorov <me@albi.io>
2024-09-02 13:33:10 +0700

    Bash-like navigation

--- zsh/configs/95-keybindings.zsh
+++ zsh/configs/95-keybindings.zsh
@@ -1,23 +1,7 @@
-autoload -Uz match-words-by-style
-
-# Определение функции для перемещения по словам и спецсимволам
-forward-word-match() {
-    zle -f match
-    autoload -Uz match-words-by-style
-    match-words-by-style -w shell -m default
-}
-
-# Функция для перемещения назад по словам и спецсимволам
-backward-word-match() {
-    zle -f match
-    autoload -Uz match-words-by-style
-    match-words-by-style -w shell -m default -b
-}
-zle -N forward-word-match
-zle -N backward-word-match
-
 bindkey -e
 setopt EMACS
+autoload -U select-word-style
+select-word-style bash
 
 # handy keybindings
 bindkey "^K"      kill-whole-line                      # ctrl-k
@@ -30,5 +14,3 @@ bindkey "^N"      insert-last-word                     # ctrl-n
 bindkey "^D"      delete-char                          # ctrl-d
 bindkey "^F"      forward-char                         # ctrl-f
 bindkey "^B"      backward-char                        # ctrl-b
-bindkey "^[[1;4F" forward-word-match
-bindkey "^[[1;4B" backward-word-match
