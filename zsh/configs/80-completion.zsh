6acb36bb8121eea5a4a8d2fa8c29a73a7d6505c8
Albert Kiskorov <albert@kiskorov.org>
2024-12-21 23:39:17 +0700

    carapace autocomplete

--- zsh/configs/80-completion.zsh
+++ zsh/configs/80-completion.zsh
@@ -4,4 +4,7 @@ fpath=(/opt/vagrant/embedded/gems/gems/vagrant-2.4.0/contrib/zsh $fpath)
 fpath=(/opt/homebrew/Cellar/helix/*/share/zsh/site-functions $fpath)
 fpath=(/opt/homebrew/Cellar/m-cli/*/share/zsh/site-functions $fpath)
 fpath=($(brew --prefix)/share/zsh/site-functions $fpath)
+export CARAPACE_BRIDGES='zsh,fish,bash,inshellisense'
+zstyle ':completion:*' format $'\e[2;37mCompleting %d\e[m'
+source <(carapace _carapace)
 export -U fpath
