0ec032643018be5065fbcda0a6d14674b1730306
Albert Kiskorov <albert@kiskorov.org>
2025-02-19 21:27:56 +0700

    [sketchybar] write lua errors to stderr, and enable lua stdout

--- config/sketchybar/init.lua
+++ config/sketchybar/init.lua
@@ -1,16 +1,37 @@
 -- Require the sketchybar module
 sbar = require("sketchybar")
 
+-- Patch sbar.exec to output errors from stderr
+local original_exec = sbar.exec
+sbar.exec = function(command, callback)
+  local function my_callback(stdout, stderr)
+    if stderr and stderr ~= "" then
+      -- Print errors to stderr
+      io.stderr:write("Error executing command '" .. command .. "': " .. stderr .. "\n")
+    end
+    if callback then callback(stdout, stderr) end
+  end
+  original_exec(command, my_callback)
+end
+
 -- Set the bar name, if you are using another bar instance than sketchybar
 -- sbar.set_bar_name("bottom_bar")
 
--- Bundle the entire initial configuration into a single message to sketchybar
+-- Begin configuration
 sbar.begin_config()
-require("bar")
-require("default")
-require("items")
+
+-- Wrap configuration requires in an xpcall to catch startup errors.
+local ok, err = xpcall(function()
+  require("bar")
+  require("default")
+  require("items")
+end, debug.traceback)
+
+if not ok then
+  io.stderr:write("Error in configuration: " .. err .. "\n")
+end
+
 sbar.end_config()
 
--- Run the event loop of the sketchybar module (without this there will be no
--- callback functions executed in the lua module)
+-- Run the event loop of the sketchybar module
 sbar.event_loop()
