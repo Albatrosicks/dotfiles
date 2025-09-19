d18822fef76b3dae9466169e0d4bbaa83c125422
Albert Kiskorov <albert@kiskorov.org>
2025-09-19 21:09:13 +0700

    Remove zellij on fish startup

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -28,17 +28,6 @@ end
 #     # set ZELLIJ_AUTO_EXIT true
 #     eval (zellij setup --generate-auto-start fish | string collect)
 # end
-if not set -q ZELLIJ
-    if test "$ZELLIJ_AUTO_ATTACH" = true
-        zellij attach -c
-    else
-        zellij
-    end
-
-    if test "$ZELLIJ_AUTO_EXIT" = true
-        kill $fish_pid
-    end
-end
 
 # Load custom executable functions
 for function_file in ~/.config/fish/functions/*
