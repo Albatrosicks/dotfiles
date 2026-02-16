d134428e35233e6a87775436f3d65b1f66f4cb95
Albert Kiskorov <albert@kiskorov.org>
2026-02-16 17:53:57 +0700

    [ansible] Better vault pass script

--- config/ansible/vault-pass.sh
+++ config/ansible/vault-pass.sh
@@ -1,2 +1,13 @@
-#!/bin/bash
-security find-generic-password -s "ansible-vault" -a "$USER" -w | tr -d '\n'
+#!/usr/bin/env bash
+
+[[ -n $(logname >/dev/null 2>&1) ]] && logged_in_user=$(logname) || logged_in_user=$(whoami)
+
+
+if [[ ! $(uname) = "Darwin" ]]; then
+	echo "This script is for macOS only."
+	exit 1
+fi
+
+security find-generic-password \
+  -a $logged_in_user \
+  -s ansible-vault -w
