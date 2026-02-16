5404f2fa4339a5476bac4f723217d33dcbd4a00c
Albert Kiskorov <albert@kiskorov.org>
2026-02-16 18:26:11 +0700

    [ansible] Some unnecessary fixes for vault pass file

--- config/ansible/vault-pass.sh
+++ config/ansible/vault-pass.sh
@@ -1,13 +1,45 @@
 #!/usr/bin/env bash
 
+# $USER
 [[ -n $(logname >/dev/null 2>&1) ]] && logged_in_user=$(logname) || logged_in_user=$(whoami)
 
-
+# check os
 if [[ ! $(uname) = "Darwin" ]]; then
 	echo "This script is for macOS only."
 	exit 1
 fi
 
-security find-generic-password \
-  -a $logged_in_user \
-  -s ansible-vault -w
+add_keychain_password() {
+	local mypass
+	read -s -p "Enter ansible vault password: " mypass
+	security add-generic-password \
+		-a "$logged_in_user" \
+		-s "ansible-vault" \
+		-w "$mypass" \
+		-T "/usr/bin/security"
+}
+
+check_app_password() {
+	app_password=$(security find-generic-password \
+		-a "$logged_in_user" \
+		-s "ansible-vault" -w 2>&1 >/dev/null)
+	rc=$(echo $?)
+	if [[ $rc -ne 0 ]]; then
+		echo "No password found in keychain. "
+		add_keychain_password
+	fi
+}
+
+print_vault_password() {
+	security find-generic-password \
+		-a $logged_in_user \
+		-s ansible-vault -w 2>/dev/null | tr -d '\n'
+}
+
+main() {
+	check_app_password
+	print_vault_password
+}
+main
+
+exit 0
--- config/fish/conf.d/10-path.fish
+++ config/fish/conf.d/10-path.fish
@@ -13,7 +13,7 @@ end
 set -gx GOPATH ~/go
 
 # Set Ansible vault password file
-set -Ux ANSIBLE_VAULT_PASSWORD_FILE ~/.config/ansible/vault-pass.sh
+set -Ux ANSIBLE_VAULT_PASSWORD_FILE /Users/$USER/.config/ansible/vault-pass.sh
 
 # Build PATH with proper precedence (first entries have highest priority)
 set -gx PATH \
